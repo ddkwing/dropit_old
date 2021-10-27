47e7e0251cc52f1d5969d04068e49b5bcda8ef8b
kalvin <daidekun@tal.com>
2021-10-27 16:04:35 +0800

    Navigation Guard

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from tortoise import Tortoise  # NEW
+from tortoise import Tortoise
 
 from src.database.register import register_tortoise
 from src.database.config import TORTOISE_ORM
 
-
 # enable schemas to read relationship between models
-Tortoise.init_models(["src.database.models"], "models")  # NEW
+Tortoise.init_models(["src.database.models"], "models")
+
+"""
+import 'from src.routes import users, notes' must be after 'Tortoise.init_models'
+why?
+https://stackoverflow.com/questions/65531387/tortoise-orm-for-python-no-returns-relations-of-entities-pyndantic-fastapi
+"""
+from src.routes import users, notes
 
 app = FastAPI()
 
@@ -18,9 +24,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(users.router)
+app.include_router(notes.router)
 
 register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
-
 
 @app.get("/")
 def home():
--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
 
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 
 axios.defaults.withCredentials = true;
@@ -13,5 +14,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
