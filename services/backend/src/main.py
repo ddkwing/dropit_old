6cc5356658cc0e82caac4561603adc50867111f6
kalvin <daidekun@tal.com>
2021-10-27 12:41:41 +0800

    add

---
+++ services/backend/src/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from tortoise import Tortoise  # NEW
+
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM
+
+
+# enable schemas to read relationship between models
+Tortoise.init_models(["src.database.models"], "models")  # NEW
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
+
+
+@app.get("/")
+def home():
+    return "Hello, World!"
